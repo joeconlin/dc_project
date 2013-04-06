87ff6d01ac4667da828f77d31476a778ae926444
Joe Conlin <joe@joeconlin.org>
2013-04-06 15:17:44 -0700

    updated models

--- dcbuilder/admin.py
+++ dcbuilder/admin.py
@@ -1,6 +1,26 @@
+from django.forms import ModelForm
 from django.contrib import admin
-from dcbuilder.models import Environment,Space,Power,Network,Firewall,LoadBalancer,Switch,Router,Server,Storage,Application
+from django.contrib.admin import ModelAdmin
+from dcbuilder.models import Environment,Space,Power,Network,Firewall,LoadBalancer,Switch,Router,Server,Storage,Application,Proposal,Customer
+from suit_redactor.widgets import RedactorWidget
 
+class PageForm(ModelForm):
+    class Meta:
+        widgets = {
+            'detail': RedactorWidget(editor_options={'lang': 'en'})
+        }
+
+class ProposalAdmin(ModelAdmin):
+    form = PageForm
+    fieldsets = [
+        ('General', {
+            'fields': ('name', 'customer', 'environment')
+        }),
+      ('Detail Fieldset', {'classes': ('full-width',), 'fields': ('detail',)})
+    ]
+
+admin.site.register(Proposal, ProposalAdmin)
+admin.site.register(Customer)
 admin.site.register(Environment)
 admin.site.register(Space)
 admin.site.register(Power)
