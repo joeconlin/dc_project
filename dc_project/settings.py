25c7be8f07181ea6756f8b5441e015bc2d836c72
Joe Conlin <joe@joeconlin.org>
2013-04-06 10:48:08 -0700

    minor update to models

--- dc_project/settings.py
+++ dc_project/settings.py
@@ -126,6 +126,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
     'dcbuilder',
     'suit',
     # Uncomment the next line to enable the admin:
