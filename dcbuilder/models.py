a96e57adeb783c1739efebabe45273d7bb4decca
Joe Conlin <joe@joeconlin.org>
2013-04-06 07:29:53 -0700

    outlined models and active admin

--- dcbuilder/models.py
+++ dcbuilder/models.py
@@ -1,39 +1,80 @@
 # vim: ai ts=4 sts=4 et sw=4
 from django.db import models
 
-class Environment(models.Model):
-    name = models.CharField('Environment Name', max_length=100, help_text="What is the name of this")
-    space = 
-    power =
-    network =
+class Space(models.Model):
+    name = models.CharField('Name', max_length=100)
 
     def __unicode__(self):
         return self.name
 
+class Power(models.Model):
+    name = models.CharField('Name', max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
+
 class Network(models.Model):
+    name = models.CharField('Name', max_length=100)
+
+    def __unicode__(self):
+        return self.name
 
 class Firewall(models.Model):
+    name = models.CharField('Name', max_length=100)
+
+    def __unicode__(self):
+        return self.name
     
 class LoadBalancer(models.Model):
+    name = models.CharField('Name', max_length=100)
+
+    def __unicode__(self):
+        return self.name
 
 class Switch(models.Model):
+    name = models.CharField('Name', max_length=100)
+
+    def __unicode__(self):
+        return self.name
 
 class Router(models.Model):
+    name = models.CharField('Name', max_length=100)
+
+    def __unicode__(self):
+        return self.name
 
 class Server(models.Model):
+    name = models.CharField('Name', max_length=100)
+
+    def __unicode__(self):
+        return self.name
 
 class Storage(models.Model):
+    name = models.CharField('Name', max_length=100)
 
-class Interface(models.Model):
+    def __unicode__(self):
+        return self.name
+
+#class Interface(models.Model):
 
 class Application(models.Model):
+    name = models.CharField('Name', max_length=100)
 
+    def __unicode__(self):
+        return self.name
 
-
-class StorageArea(models.Model):
-        name = models.CharField('Area Name', max_length=100, help_text="What is the name of this ")
-        created_date = models.DateTimeField('Date record created', auto_now_add=True)
-        updated = models.DateTimeField( auto_now=True)
-        def __unicode__(self):
-           return self.name
-
+class Environment(models.Model):
+    name = models.CharField('Environment Name', max_length=100, help_text="What is the name of this")
+    space = models.ForeignKey(Space, verbose_name="Linked Space")
+    power = models.ForeignKey(Power, verbose_name="Linked Power")
+    network = models.ForeignKey(Network, verbose_name='Linked Network')
+    firewall = models.ForeignKey(Firewall, verbose_name="Linked Firewall")
+    loadbalancer = models.ForeignKey(LoadBalancer, verbose_name="Linked LB")
+    switch = models.ForeignKey(Switch, verbose_name="Linked Switch")
+    router = models.ForeignKey(Router, verbose_name="Linked Router")
+    server = models.ForeignKey(Server, verbose_name="Linked Server")
+    storage = models.ForeignKey(Storage, verbose_name="Linked Storage")
+    application = models.ForeignKey(Application, verbose_name="Linked App")
+    def __unicode__(self):
+        return self.name
