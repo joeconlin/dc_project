09865002df65cce0b38b2cb702470d8cf34663f0
Joe Conlin <joe@joeconlin.org>
2013-04-06 15:25:46 -0700

    updated db settings

--- dc_project/settings.py
+++ dc_project/settings.py
@@ -20,8 +20,8 @@ DATABASES = {
         #'NAME': 'dcbuilder.db',                      # Or path to database file if using sqlite3.
         'NAME': 'dc_builder',
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'omgl0lz',
+        'USER': 'dc_project_user',
+        'PASSWORD': 'loldongs',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
